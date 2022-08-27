a0418296b31cd390b256b73ebfd71fdf74a18d8a
valentinblanco1 <blanco.valentin.epet20@gmail.com>
2022-08-27 14:32:23 -0300

    agregacion de carpetas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Login from './pages/Login/Login';
+
 function App() {
   return (
     <div className="App">
-      hola
+      <Login />
     </div>
   );
 }
---
+++ src/pages/Login/componentes/Title/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+    return (
+        <div>
+            APP TRIMESTRE 
+        </div>
+    )
+};  
+
+export default Title;
